a56d35ccc824bc7aad4bce2c7fd0c734ee6d7fbc
Pavani1006 <pavanivallem@gmail.com>
2024-10-01 20:49:40 +0530

    firat commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import bg1 from '../assets/bg2.jpeg';
 import './Home.css';
 
 const Home = () => {
-  const driveLink = "https://drive.google.com/file/d/1ykmrvhjTigte1WGx7recJCp-1c9EoH78/view?usp=drive_link";
+  const driveLink = "https://drive.google.com/file/d/1EPl64fd3SfYhl6EvdNxN73xo30HveOmz/view?usp=drivesdk";
 
   const handleClick = () => {
     window.open(driveLink, '_blank', 'noopener,noreferrer');
